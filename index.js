19dadc112eb765d84c585fe578c8136786fa56a9
somrathumayan <somrathumayan@gmail.com>
2025-01-19 22:10:20 +0600

    declare variables

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ console.log("Hello World");
 /*
 Md. Humayan Kabir
 Started Learning Javascript with free codecamp
-
 */
+
+// data types: 
+// undefined, null, boolean, string, symbol, number and object 
+
+var name = "Humayan";
+let num = 12;
+const pie = 3.1416;
