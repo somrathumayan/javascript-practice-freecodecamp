2cbc4bb262247359a99eb2952e400666d9e5a61f
somrathumayan <somrathumayan@gmail.com>
2025-01-19 22:12:17 +0600

    assigned variable

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ Started Learning Javascript with free codecamp
 // data types: 
 // undefined, null, boolean, string, symbol, number and object 
 
-var name = "Humayan";
-let num = 12;
-const pie = 3.1416;
+// var name = "Humayan";
+// let num = 12;
+// const pie = 3.1416;
+
+// assignment operator
+var a = 7;
+var b = 8;
+
+// a assigned to 7
