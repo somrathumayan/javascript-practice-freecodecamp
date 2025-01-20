a8112192133c922b316c0de5621645df59630d86
somrathumayan <somrathumayan@gmail.com>
2025-01-20 22:47:21 +0600

    create object and get its value

--- index.js
+++ index.js
@@ -83,3 +83,16 @@ function abTest(p, q){
 }
 
 console.log(abTest(2 , 2));
+
+// create object
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats",
+} 
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
+
+console.log(hatValue);
+console.log(shirtValue);
